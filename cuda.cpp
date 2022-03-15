224d494476559880507db01edeb16b09ede821d4
Leorius Malz <75719027+LeoriusMalz@users.noreply.github.com>
2022-03-15 14:41:11 +0300

    Create cuda.cpp

---
+++ cuda.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    FILE *f1;
+    FILE *f2;
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    int cash = 0;
+    int pash = 0;
+
+    freopen("cuda.in", "r", stdin);
+    freopen("cuda.out", "w", stdout);
+
+    f1 = fopen("cuda.in", "r");
+    scanf("%d%d%d", &a, &b, &c);
+    fclose(f1);
+
+    int A = ((a-1)/32 + 1)*32;
+    int B = ((b-1)/2 + 1)*2;
+    int C = ((c-1)/128 + 1)*128;
+
+    cash = 1536 / A;
+    pash = 32768 / (A*B);
+
+    if (pash < cash)
+        cash = pash;
+
+    pash = 49152 / C;
+
+    if (pash < cash)
+        cash = pash;
+
+    int cuda = (int)round(((cash * a)/1536.0)*100);
+
+    f2 = fopen("cuda.out", "w");
+    printf("%d", cuda);
+    fclose(f2);
+
+    return 0;
+}
